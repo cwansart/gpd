fc4daa003c8de5b93acb2aa3b033d6fa979cb8f2
Larissa Schenk <larissa.schenk@hs-osnabrueck.de>
2017-06-12 07:11:29 +0200

    bugfix

--- src/OgdfTest.cpp
+++ src/OgdfTest.cpp
@@ -6,10 +6,24 @@
 using namespace ogdf;
 using namespace nlohmann;
 
+/**
+* Constructor, calls JSONMachineToGraph to convert the machine to an ogdf::Graph
+*
+* @param    JSONstrng, machine as a JSON string
+* @author   laschenk
+* @since    2017-06-08
+*/
 OgdfTest::OgdfTest(std::string &JSONstrng) {
-    JSONMachineToGraph(JSONstrng);
+    //JSONMachineToGraph(JSONstrng);
 }
 
+/**
+* Converts the machine to a processable ogdf::Graph (ignores i.e. recursive transitions)
+*
+* @param    JSONstrng, machine as a JSON string
+* @author   laschenk
+* @since    2017-06-08
+*/
 void OgdfTest::JSONMachineToGraph(std::string &JSONstrng){
     json j = json::parse(JSONstrng);
 
@@ -39,13 +53,20 @@ void OgdfTest::JSONMachineToGraph(std::string &JSONstrng){
     }
 }
 
+/**
+* Proofs if the graph is (not) planar, creates a (mostly) planar layout and new positions for the nodes
+*
+* @author   laschenk
+* @since    2017-06-08
+*/
 void OgdfTest::graphToPlanarRep(){
     GA = GraphAttributes(G);
     GA.addAttributes(GraphAttributes::edgeType);
     GA.addAttributes(GraphAttributes::nodeType);
+    randomSimpleGraph(G, 10, 20);
 
     BoyerMyrvold BM;
-    if (BM.isPlanar(G) && G.numberOfNodes()) {
+    if (BM.isPlanar(G)) {
         PlanarStraightLayout PSL;
         PSL.call(GA);
 
@@ -64,10 +85,18 @@ void OgdfTest::graphToPlanarRep(){
     }
 }
 
-// INFO: Mention that we only need the new positions of the states/nodes
-// and the ID of every node. So we can change the position of every
-// node in the toolbox.
+/**
+* Saves the new node-positions in a JSON string, that is send to the toolbox later
+*
+* @author   laschenk
+* @since    2017-06-08
+* @return   string, the JSON string
+*/
 std::string OgdfTest::planarRepToJSON(){
+    // INFO: Mention that we only need the new positions of the states/nodes
+    // and the ID of every node. So we can change the position of every
+    // node in the toolbox.
+
     int counter = 0;
     json j;
     j["states"] = json::array();
@@ -76,9 +105,14 @@ std::string OgdfTest::planarRepToJSON(){
         j["states"][counter] = { { "id", vG->index() }, {"xPos:", GA.x(vG)},  {"yPos:", GA.y(vG)}};
         counter++;
     }
-
     return j.dump();
 }
+
+/**
+ *
+ * Helper methods
+ * @author laschenk
+ */
 
 node *OgdfTest::getNodeByIndex(int index){
     node *n = NULL;
